885af938cb701c4875296791af2bd0de0b628595
Yorune <naki2642@gmail.com>
2026-05-19 22:50:50 +0800

    Stabilize CU grid mask at high zoom

--- native/video_renderer/shaders/analysis_overlay_mask_rect.hlsl
+++ native/video_renderer/shaders/analysis_overlay_mask_rect.hlsl
@@ -99,18 +99,41 @@ VSMaskOutput VSMain(uint vertex_id : SV_VertexID, uint instance_id : SV_Instance
         clipped_max = clipped_min;
     }
 
-    float2 local_uv = lerp(clipped_min, clipped_max, rect_corner(vertex_id));
-    float2 global_uv = overlay_global_uv_from_local_uv(local_uv, track_idx);
+    const float line_width_px = 2.0;
+    float2 clipped_global_min = overlay_global_uv_from_local_uv(clipped_min, track_idx);
+    float2 clipped_global_max = overlay_global_uv_from_local_uv(clipped_max, track_idx);
+    float2 visible_global_min = overlay_global_uv_from_local_uv(visible_rect.xy, track_idx);
+    float2 visible_global_max = overlay_global_uv_from_local_uv(visible_rect.zw, track_idx);
+    float2 clipped_px_min = min(clipped_global_min, clipped_global_max) *
+        float2(u_canvas_width, u_canvas_height);
+    float2 clipped_px_max = max(clipped_global_min, clipped_global_max) *
+        float2(u_canvas_width, u_canvas_height);
+    float2 visible_px_min = min(visible_global_min, visible_global_max) *
+        float2(u_canvas_width, u_canvas_height);
+    float2 visible_px_max = max(visible_global_min, visible_global_max) *
+        float2(u_canvas_width, u_canvas_height);
+    float2 draw_px_min = max(clipped_px_min - line_width_px, visible_px_min);
+    float2 draw_px_max = min(clipped_px_max + line_width_px, visible_px_max);
+    float2 corner_px = lerp(draw_px_min, draw_px_max, rect_corner(vertex_id));
     float2 rect_global_min = overlay_global_uv_from_local_uv(local_rect.xy, track_idx);
     float2 rect_global_max = overlay_global_uv_from_local_uv(local_rect.zw, track_idx);
+    float4 rect_px = float4(
+        min(rect_global_min.x, rect_global_max.x) * u_canvas_width,
+        min(rect_global_min.y, rect_global_max.y) * u_canvas_height,
+        max(rect_global_min.x, rect_global_max.x) * u_canvas_width,
+        max(rect_global_min.y, rect_global_max.y) * u_canvas_height);
 
     VSMaskOutput output;
-    output.position = float4(global_uv.x * 2.0 - 1.0, 1.0 - global_uv.y * 2.0, 0.0, 1.0);
+    output.position = float4(
+        corner_px.x / u_canvas_width * 2.0 - 1.0,
+        1.0 - corner_px.y / u_canvas_height * 2.0,
+        0.0,
+        1.0);
     output.rect_px = float4(
-        rect_global_min.x * u_canvas_width,
-        rect_global_min.y * u_canvas_height,
-        rect_global_max.x * u_canvas_width,
-        rect_global_max.y * u_canvas_height);
+        floor(rect_px.x + 0.5),
+        floor(rect_px.y + 0.5),
+        floor(rect_px.z + 0.5),
+        floor(rect_px.w + 0.5));
     output.line_strength = line_strength_from_rect(rect);
     output.rect_uv = float4(rect_min, rect_max);
     return output;
@@ -122,16 +145,24 @@ float4 PSMain(VSMaskOutput input) : SV_TARGET {
     }
 
     float2 px = input.position.xy;
-    float left = px.x - input.rect_px.x;
-    float top = px.y - input.rect_px.y;
-    float right = input.rect_px.z - px.x;
-    float bottom = input.rect_px.w - px.y;
-    const float line_width_px = 0.75;
+    float left = input.rect_px.x;
+    float top = input.rect_px.y;
+    float right = input.rect_px.z;
+    float bottom = input.rect_px.w;
+    if (right <= left || bottom <= top) {
+        discard;
+    }
+
+    const float line_width_px = 2.0;
+    bool within_y = px.y >= top && px.y < bottom;
+    bool within_x = px.x >= left && px.x < right;
     bool on_line =
-        left <= line_width_px ||
-        top <= line_width_px ||
-        (input.rect_uv.z >= 0.9999 && right <= line_width_px) ||
-        (input.rect_uv.w >= 0.9999 && bottom <= line_width_px);
+        (within_y && px.x >= left && px.x < left + line_width_px) ||
+        (within_x && px.y >= top && px.y < top + line_width_px) ||
+        (input.rect_uv.z >= 0.9999 &&
+            within_y && px.x >= right - line_width_px && px.x < right) ||
+        (input.rect_uv.w >= 0.9999 &&
+            within_x && px.y >= bottom - line_width_px && px.y < bottom);
     if (!on_line) {
         discard;
     }
